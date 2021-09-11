0e7245f24aac04196c4dd0edb0a3b7f763cbf27f
tadashim <ma.tadashi@gmail.com>
2021-09-11 17:42:33 +0900

    add Home and New Fundraiser components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Typography from '@material-ui/core/Typography';
 import FundraiserFactoryContract from "./contracts/FundraiserFactory.json";
 import getWeb3 from "./getWeb3";
+import NewFundraiser from "./NewFundraiser";
+import Home from "./Home";
 import "./App.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
 
 const App = () => {
   const [state, setState] = useState({ web3: null, accounts: null, contract: null });
+  const classes = useStyles();
 
   useEffect(() => {
     const init = async () => {
@@ -34,9 +48,21 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Fundraiser</h1>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <ToolBar>
+            <Typography variant="h6" color="inherit">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </Typography>
+            <NavLink className="nav-link" to="/new/">New</NavLink>
+          </ToolBar>
+        </AppBar>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/new" exact component={NewFundraiser} />
+      </div>
+    </Router>
   );
 }
 
